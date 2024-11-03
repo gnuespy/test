85a111f59eeed8c70f58c996512bc569268339dd
gnuespy <15sally51@naver.com>
2024-11-03 17:56:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,24 @@
-from google.cloud import secretmanager, bigquery
-import json
+from google.cloud import bigquery
+from google.oauth2 import service_account
 import streamlit as st
+import json
 
-# 1. Secret Manager에서 서비스 계정 키를 불러오는 함수
-def get_service_account_key(secret_id="moviejj", project_id="semiotic-vial-440207-q4"):
-    client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
-    response = client.access_secret_version(name=name)
-    service_account_info = response.payload.data.decode("UTF-8")
-    return json.loads(service_account_info)
-
-# 2. BigQuery 클라이언트 설정 함수
+# 1. BigQuery 클라이언트 생성 함수
 def get_bq_client():
-    service_account_key = get_service_account_key()
-    client = bigquery.Client.from_service_account_info(service_account_key)
+    # Streamlit Secrets Manager에서 서비스 계정 키를 불러옵니다
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"]
+    )
+    client = bigquery.Client(credentials=credentials, project=credentials.project_id)
     return client
 
-# 3. BigQuery에서 데이터 조회 함수
+# 2. BigQuery에서 데이터 조회 함수
 def load_data(query):
     client = get_bq_client()
     query_job = client.query(query)
     return query_job.result().to_dataframe()
 
-# 4. BigQuery에 JSON 데이터를 업데이트하는 함수
+# 3. BigQuery에 JSON 데이터를 업데이트하는 함수
 def update_bigquery_table(data):
     client = get_bq_client()
     table_id = "semiotic-vial-440207-q4.json_test.usertest"  # BigQuery 테이블 ID 반영
@@ -32,7 +28,7 @@ def update_bigquery_table(data):
     else:
         st.error(f"업데이트 중 오류 발생: {errors}")
 
-# 5. Streamlit 인터페이스 설정
+# 4. Streamlit 인터페이스 설정
 # BigQuery에서 데이터를 조회하고 화면에 표시
 query = "SELECT * FROM `semiotic-vial-440207-q4.json_test.usertest` LIMIT 100"  # 쿼리를 원하는 대로 변경
 data = load_data(query)
